240a63c14fb22632874e04f84319bffdf8201a9f
Thiago Peralta <tcperalta2006+github@gmail.com>
2026-04-15 17:38:10 -0300

    Implementação da função acordo mutuo

--- src/mknn.py
+++ src/mknn.py
@@ -12,3 +12,44 @@ def k_vizinhos(matrix, k):
             x+=1
         tdsVizinhos.append(nn)
     return(tdsVizinhos)
+
+def criaMatrVazia(n):
+    i=0
+    matrix = []
+    while(i<n):
+        j = 0
+        linha = []
+        while(j<n):
+            linha.append(0)
+            j+=1
+        i+=1
+        matrix.append(linha)
+    return matrix
+    
+def acordoMutuo(favoritos):
+    n = len(favoritos)
+    matrix = criaMatrVazia(n)
+    for i in range(n):
+        for j in range(n):
+            if ((i in favoritos[j]) and (j in favoritos[i])):
+                matrix[i][j] = 1
+    return matrix
+
+matriz_teste = [
+        [10.0, 2.5, 8.1, 4.0],
+        [2.5, 0.0, 5.2, 7.3],
+        [8.1, 5.2, 0.0, 1.1],
+        [4.0, 7.3, 1.1, 0.0]]
+
+k = 2
+favoritos = k_vizinhos(matriz_teste, k)
+
+print("--- LISTA DE FAVORITOS (K-NN) ---")
+for i in range(len(favoritos)):
+    print(f"Ponto {i} escolheu: {favoritos[i]}")
+
+matriz_adjacencia = acordoMutuo(favoritos)
+
+print("\n--- MATRIZ DE ADJACÊNCIA (ACORDO MÚTUO) ---")
+for i in range(len(matriz_adjacencia)):
+    print(f"Ponto {i} -> {matriz_adjacencia[i]}")
